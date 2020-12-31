f56f04b95fcfcfaf7fa50e516c6c2f25c6fdcdea
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2020-12-31 10:15:38 +0530

    Initial Commit

---
+++ AdharCardManagementSystemJB/src/main/java/com/aadhar/app/AdharCardManagementSystemJbApplication.java
@@ -0,0 +1,13 @@
+package com.aadhar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdharCardManagementSystemJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdharCardManagementSystemJbApplication.class, args);
+	}
+
+}
---
+++ AdharCardManagementSystemJB/src/test/java/com/aadhar/app/AdharCardManagementSystemJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadhar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdharCardManagementSystemJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
